5acac162a0ad5f3ea8cb0dd5ed0a5ae62be0d50f
Rodrigo Silva <rodrigo.silva@solucoesglobais.com.br>
2014-11-20 16:31:48 -0200

    adicionado google analytics

--- application/views/footer.php
+++ application/views/footer.php
@@ -11,5 +11,15 @@
         <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/modernizr/modernizr.js"></script>
         <!--<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/l10n/l10n.js"></script>-->
         <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/require.js/require.js" data-main="<?php echo base_url(); ?>assets/js/app"></script>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-56961719-1', 'auto');
+            ga('send', 'pageview');
+
+        </script>
     </body>
 </html>
